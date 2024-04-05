e373504f1cfb9f61c057e289f6ab70406951b9a3
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-04-05 13:29:26 -0600

    Hacer unsubscribe del Observable debouncer.

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,27 +1,30 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent implements OnInit{
+export class SearchBoxComponent implements OnInit, OnDestroy{
 
   private debouncer: Subject<string> = new Subject<string>;
-
+  private debouncerSuscrition?: Subscription;
   @Input()
   public placeholder: string = '';
   @Output()
   public onValue: EventEmitter< string > = new EventEmitter <string> ();
 
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSuscrition = this.debouncer
     .pipe(
       debounceTime(300)
     )
     .subscribe( value => {
       this.onValue.emit( value );
     });
+  }
+  ngOnDestroy(): void {
+    this.debouncerSuscrition?.unsubscribe();
   }
 
 
